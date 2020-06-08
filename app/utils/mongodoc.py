02dfaa9fcfd856e531c9eddc4b35c5c5a8037303
Moody Rahman <mrahman01@stuy.edu>
2020-06-08 17:30:15 -0400

    Essay model + renamed import

--- app/utils/mongodoc.py
+++ app/utils/mongodoc.py
@@ -1,4 +1,26 @@
-import mongoengine
+import mongoengine as mgng
+from mongoengine import connect
+
+connect('sitedata', host="moody.wtf", port=27017)
+
+class Essay(mgng.EmbeddedDocument):
+    timestamp = mgng.DateTimeField()
+    body = mgng.StringField()
+    question = mgng.StringField()
+    
+#     wordfrequency = mgng.DictField()  # {String letter: int occurence}  # to be calculated when essay loaded, not in DB
+
+#     meansentencelength = mgng.FloatField()
+#     wordcount = mgng.IntField()
+#     maxwordcount = mgng.IntField()
+#     metapercentile = mgng.IntField()
+#     ^^ some of the expected values ^^
+    essaystats = mgng.DictField()
+
+
+    ispublic = mgng.BooleanField()
+
+
 
 
 class Essay(mongoengine.Document):
