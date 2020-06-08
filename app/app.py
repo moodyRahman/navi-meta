3b875680b544808c49f76a0afcd6be23ebf83394
Ivan Galakhov <igalakhov.nyc@gmail.com>
2020-06-08 10:49:24 -0400

    ?

--- app/app.py
+++ app/app.py
@@ -1,18 +1,19 @@
-#Yevgeniy Gorbachev
-#SoftDev1 pd1
-#K<n> -- <K<n>.__name__>
-#ISO 8601 Date
+# Yevgeniy Gorbachev
+# SoftDev1 pd1
+# K<n> -- <K<n>.__name__>
+# ISO 8601 Date
 
 from flask import *
 from os import urandom
 
-
 app = Flask(__name__)
 app.secret_key = urandom(32)
+
 
 @app.route('/')
 def index():
     return render_template('_base.html')
+
 
 application = app
 if __name__ == '__main__':
