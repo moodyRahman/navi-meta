18f8d5044e4d97a12953eeacfeb1452e9e641595
Moody Rahman <mrahman01@stuy.edu>
2020-06-08 19:09:35 -0400

    utilizes mongodoc

--- app/utils/usrctl.py
+++ app/utils/usrctl.py
@@ -1,19 +1,22 @@
 from ..utils import dbctl
 from re import match
+from ..utils import mongodoc
 
 name_form = '\w{3,20}'
 
 
 def login(name, password):
     '''
-    Checks name and password against database contents
+    Check name and password against database contents
 
     If successful, returns a dictionary containing user information.
 
     Otherwise returns `False`.
     '''
 
-    user = dbctl.find(dbctl.users, {'name': name})
+    # user = dbctl.find(dbctl.users, {'name': name})
+    # print(type())
+    user=mongodoc.User.objects(name=name)
     if not user or user[0]['password'] != password:
         return False
     else:
@@ -28,13 +31,16 @@ def create(name, password, mode='user'):
     '''
 
     if match(name_form, name):
-        dupes = dbctl.find(dbctl.users, {'name': name})
+        # dupes = dbctl.find(dbctl.users, {'name': name})
+        dupes = mongodoc.User.objects(name=name)
         if dupes:
             raise ValueError('Username already exists')
-        uid = dbctl.store(
-            dbctl.users,
-            {'name': name, 'password': password, 'mode': mode}
-        )
+        # uid = dbctl.store(
+        #     dbctl.users,
+        #     {'name': name, 'password': password, 'mode': mode}
+        # )
+        newuser = mongodoc.User(name=name, password=password, accounttype=mode)
+        newuser.save()
     else:
         raise ValueError(
             'Username must be from 3 to 20 characters long, consisting only of alphanumeric characters and underscores')
